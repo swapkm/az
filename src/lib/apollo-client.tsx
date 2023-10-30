a7a598e6ae3df57999746c79db48746fb0b85401
SwapKam <swapkx@gmail.com>
2023-10-30 09:09:53 +0530

    ok

--- src/lib/apollo-client.tsx
+++ src/lib/apollo-client.tsx
@@ -5,6 +5,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 export const Providers = ({ children }: { children: any }) => {
 	const client = new ApolloClient({
 		uri: "https://az-swapkam.cyclic.app",
+		//uri: "http://localhost:8000",
 		cache: new InMemoryCache(),
 	});
 	return <ApolloProvider client={client}>{children}</ApolloProvider>;
