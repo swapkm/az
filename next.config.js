1f93bf88e45cf7897d3eafc4f778a616a937275c
SwapKam <swapkx@gmail.com>
2023-11-03 14:39:19 +0530

    basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/az',
     reactStrictMode: true,
     swcMinify: true,
     output: 'export',
