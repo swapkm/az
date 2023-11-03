28622923a90652bb2806137c98ab3f13cf116773
SwapKam <swapkx@gmail.com>
2023-11-03 14:44:08 +0530

    update

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/az',
     reactStrictMode: true,
     swcMinify: true,
-    output: 'export',
 };
 
 module.exports = withContentlayer(nextConfig)
